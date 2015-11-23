8420eee5408e726f737d08c8a3698289ecdc2d4c
Josh <jfadem82@gmail.com>
2015-11-23 13:02:09 -0800

    Updates to working JS code

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var turnCount = 0;
 var boxes = document.querySelectorAll(".box");
+var button = document.querySelector("button");
+var winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[1,4,7],[2,5,8],[0,3,6],[1,4,7],[2,5,8]];
+
+button.addEventListener("click", resetBoard);
 
 for(var i =0; i < boxes.length; i++){
 	boxes[i].addEventListener("click", takeTurn)
@@ -12,7 +16,22 @@ function takeTurn(){
 			} else {
 				this.innerHTML = "O";
 			}
+			checkWinner(this.innerHTML)
 			turnCount++;
 			
 		}
+}
+
+function resetBoard(){
+	for(var i = 0; i < boxes.length; i++){
+		boxes[i].innerHTML = ""
+		}
+}
+
+function checkWinner(player){
+	for(var i = 0; i < winConditions.length; i++){
+		if(boxes[winConditions[i][0]].innerHTML === player && boxes[winConditions[i][1]].innerHTML === player && boxes[winConditions[i][2]].innerHTML === player){
+			alert(player + " wins!");
+		}
+	}
 }
